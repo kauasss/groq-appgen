69b55b8288a1e376eecad2a3310572d4924f2dcd
Jose Menendez <jose@definitive.io>
2024-12-17 14:54:39 -0800

    build: add shadcn/dark-mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,34 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { Toaster } from 'react-hot-toast'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Toaster } from "react-hot-toast";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "@/components/mode-toggle";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Groq Appgen',
-  description: 'Interactive HTML editor with AI generation',
-  icons: {
-    icon: '/icons/icon.png',
-  },
-}
+	title: "Groq Appgen",
+	description: "Interactive HTML editor with AI generation",
+	icons: {
+		icon: "/icons/icon.png",
+	},
+};
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        {children}
-        <Toaster position="bottom-right" />
-      </body>
-    </html>
-  )
+	return (
+		<html lang="en">
+			<body className={inter.className}>
+				<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+					{children}
+					<ModeToggle />
+					<Toaster position="bottom-right" />
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
