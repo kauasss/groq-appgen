018bfe045ef2cf929e4ef30a9d94ee9285b808f8
Jose Menendez <jose@definitive.io>
2024-12-19 14:37:05 -0800

    build: gallery

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,3 +1,4 @@
+import { getOgImageUrl } from "@/lib/utils";
 import { getGallery } from "@/server/storage";
 import Link from "next/link";
 
@@ -5,18 +6,23 @@ export default async function GalleryPage() {
 	const gallery = await getGallery();
 	return (
 		<main className="p-10">
-			<h1 className="font-montserrat text-[2em] font-light">Gallery</h1>
-			<div>
+			<h1 className="font-montserrat text-[2em] font-light mb-10">Gallery</h1>
+			<div className="flex flex-wrap gap-4">
 				{gallery.map((item) => (
 					<Link
 						href={`/apps/${item.sessionId}/${item.version}`}
 						key={item.sessionId}
-						className="flex flex-col gap-2 bg-red-500 max-w-[200px] rounded-lg overflow-hidden"
+						className="flex flex-col gap-2 bg-secondary max-w-[200px] rounded-lg overflow-hidden hover:ring-2 hover:ring-primary transition-all duration-100"
 					>
-						<div className="bg-blue-500 h-[200px] w-[200px] bg-[url('/images/placeholder.png')] bg-cover bg-center" />
-						<div>
+						<div
+							className="bg-blue-500 h-[200px] w-[200px] bg-[url('/images/placeholder.png')] bg-cover bg-center"
+							style={{
+								backgroundImage: `url(${getOgImageUrl(item.sessionId, item.version)})`,
+							}}
+						/>
+						<div className="p-4">
 							<div>{item.title}</div>
-							<div>{item.description}</div>
+							<div className="text-xs opacity-50">{item.description}</div>
 						</div>
 					</Link>
 				))}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,10 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
+}
+
+export function getOgImageUrl(sessionId: string, version: string) {
+	return `https://image.thum.io/get/https://appgen.groqlabs.com/api/apps/${sessionId}/${version}/raw`;
 }
