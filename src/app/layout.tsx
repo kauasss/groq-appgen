232f18166622c09382845722b71f908f1826bd30
Jose M <menendezpoo@gmail.com>
2024-12-21 07:40:26 -0800

    fix: maintenance mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,8 @@ import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ROOT_URL } from "@/utils/config";
 import { ModeToggle } from "@/components/mode-toggle";
-import { GoogleAnalytics } from '@next/third-parties/google';
+import { GoogleAnalytics } from "@next/third-parties/google";
+import { MAINTENANCE_MODE } from "@/lib/settings";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -35,9 +36,17 @@ export default function RootLayout({
 		<html lang="en">
 			<body className={inter.className}>
 				<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-					{children}
-					<ModeToggle />
-					<Toaster position="bottom-right" />
+					{MAINTENANCE_MODE ? (
+						<div className="text-center text-gray-500 py-8">
+							{"We're currently undergoing maintenance. We'll be back soon!"}
+						</div>
+					) : (
+						<>
+							{children}
+							<ModeToggle />
+							<Toaster position="bottom-right" />
+						</>
+					)}
 				</ThemeProvider>
 				<GoogleAnalytics gaId="G-MGQ7E93R12" />
 			</body>
